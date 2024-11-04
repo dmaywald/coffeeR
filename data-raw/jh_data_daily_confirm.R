613dd3cbaa760262073ed8742397380be94afc00
Devon Maywald <dmaywald@tamu.edu>
2024-11-03 20:46:12 -0600

    remove library() dependency in data generating files

--- data-raw/jh_data_daily_confirm.R
+++ data-raw/jh_data_daily_confirm.R
@@ -6,23 +6,12 @@
 # before adding them to the state's total
 ########
 
-#libraries
-
-if("MASS" %in% (.packages())){
-  detach("package:MASS")
-}
-
-library(tidyverse)
-library(varhandle)
-library(Hmisc)
-
-
 # read in time series data
 jh_data_total_confirm = read.csv("data-raw/time_series_covid19_confirmed_US.csv")
 
 # see if any of the time series are all <= 0
 # ... This will detect entries like NaN's
-just_dates        = data.frame(t(jh_data_total_confirm %>% select(starts_with('X'))))
+just_dates        = data.frame(t(jh_data_total_confirm %>% dplyr::select(dplyr::starts_with('X'))))
 names(just_dates) = jh_data_total_confirm$Combined_Key
 remove_data       = which(((sapply(just_dates, function(vec){all(vec <= 0)})) == TRUE))
 
@@ -33,8 +22,8 @@ rm(list = c("just_dates", 'remove_data'))
 
 # Split-apply-combine the data, summing up within state/territory total confirmed cases
 jh_data_total_confirm = jh_data_total_confirm %>%
-  group_by(Province_State) %>%
-  summarise_at(vars(starts_with('X')), sum)
+  dplyr::group_by(Province_State) %>%
+  dplyr::summarise_at(dplyr::vars(dplyr::starts_with('X')), sum)
 
 # Transpose dataframe and get appropriate column names
 jh_data_total_confirm        = data.frame(t(jh_data_total_confirm))
@@ -75,10 +64,10 @@ jh_data_daily_confirm$day_of_week = format(jh_data_daily_confirm$date, '%a')
 
 # Couple of 'states/territories' are cruise ships that provide uninformative data
 # ... They're 0 confirmed cases one day, and maximal confirmed cases the next.
-jh_data_daily_confirm = select(jh_data_daily_confirm, -contains('Princess'))
+jh_data_daily_confirm = dplyr::select(jh_data_daily_confirm, -contains('Princess'))
 
 
 # Sum up states/territories for US total
-jh_data_daily_confirm$US = jh_data_daily_confirm %>% select(-c(date, year, month, day_of_week)) %>% rowSums()
+jh_data_daily_confirm$US = jh_data_daily_confirm %>% dplyr::select(-c(date, year, month, day_of_week)) %>% rowSums()
 
 usethis::use_data(jh_data_daily_confirm, overwrite = TRUE)
